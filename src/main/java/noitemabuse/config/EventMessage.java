996347d7dd8611b8fa72ab44ba0c58afa03982e3
ruan <ruan9422@gmail.com>
2014-01-25 20:09:22 +0200

    run license.sh

--- src/main/java/noitemabuse/config/EventMessage.java
+++ src/main/java/noitemabuse/config/EventMessage.java
@@ -1,3 +1,4 @@
+/* This file is part of NoItemAbuse (GPL v2 or later), see LICENSE.md */
 package noitemabuse.config;
 
 
--- src/main/java/noitemabuse/config/Message.java
+++ src/main/java/noitemabuse/config/Message.java
@@ -1,3 +1,4 @@
+/* This file is part of NoItemAbuse (GPL v2 or later), see LICENSE.md */
 package noitemabuse.config;
 import static org.bukkit.ChatColor.*;
 import org.bukkit.entity.Player;
