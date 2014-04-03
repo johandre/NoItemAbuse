a9a1688746fb82d885645590c73340c858532fb9
ruan <ruan9422@gmail.com>
2014-04-03 14:53:49 +0200

    remove load dependency on CheckManager

--- src/main/java/noitemabuse/config/ConfigManager.java
+++ src/main/java/noitemabuse/config/ConfigManager.java
@@ -46,10 +46,6 @@ public class ConfigManager extends Manager {
     public List<String> getToggledPlayers() {
         return toggled;
     }
-    @Override
-    public boolean loadAfter(Manager manager) {
-        return manager instanceof CheckManager;
-    }
 
     @Override
     public void init() {
